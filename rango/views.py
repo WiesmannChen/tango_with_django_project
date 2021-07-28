a520a03a07e8b95b317959b675b1f2dd1300012d
Qisen Chen <2533017C@student.gla.ac.uk>
2021-07-28 17:05:43 +0100

    Chapter 4 finished

--- rango/views.py
+++ rango/views.py
@@ -10,7 +10,7 @@ from django.http import HttpResponse
 def index(request):
     # Construct a dictionary to pass to the template engine as its context
     # Note the key boldmessage matches to {{bolmessage}} in the template
-    context_dict = {'boldmessage': 'Crunchy,cream,cookie,candy,cupcake'}
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
 
     # return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
     # Return a rendered response to send to the client
