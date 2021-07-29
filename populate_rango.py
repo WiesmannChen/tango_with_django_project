a32d4229f72b70921a98db7a8231e132487292f6
Qisen Chen <2533017C@student.gla.ac.uk>
2021-07-29 18:26:54 +0100

    Chapter 6 with exercise

--- populate_rango.py
+++ populate_rango.py
@@ -1,4 +1,5 @@
 import os
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'tango_with_django_project.settings')
 
@@ -8,52 +9,64 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE',
 '''
 
 import django
+
 django.setup()
 
 from rango.models import Category, Page
 
+
 def populate():
     python_pages = [
         {'title': 'Official Python Tutorial',
-         'url':'http://docs.python.org/3/tutorial/'},
-        {'title':'How to Think like a Computer Scientist',
-         'url':'http://www.greenteapress.com/thinkpython/'},
-        {'title':'Learn Python in 10 Minutes',
-         'url':'http://www.korokithakis.net/tutorials/python/'} ]
+         'url': 'http://docs.python.org/3/tutorial/',
+         'views': 110, },
+        {'title': 'How to Think like a Computer Scientist',
+         'url': 'http://www.greenteapress.com/thinkpython/',
+         'views': 50},
+        {'title': 'Learn Python in 10 Minutes',
+         'url': 'http://www.korokithakis.net/tutorials/python/',
+         'views': 23}]
 
     django_pages = [
-        {'title':'Official Django Tutorial',
-         'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
-        {'title':'Django Rocks',
-         'url':'http://www.djangorocks.com/'},
-        {'title':'How to Tango with Django',
-         'url':'http://www.tangowithdjango.com/'} ]
+        {'title': 'Official Django Tutorial',
+         'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
+         'views': 123},
+        {'title': 'Django Rocks',
+         'url': 'http://www.djangorocks.com/',
+         'views': 234},
+        {'title': 'How to Tango with Django',
+         'url': 'http://www.tangowithdjango.com/',
+         'views': 10}]
 
     other_pages = [
-        {'title':'Bottle',
-         'url':'http://bottlepy.org/docs/dev/'},
-        {'title':'Flask',
-         'url':'http://flask.pocoo.org'} ]
+        {'title': 'Bottle',
+         'url': 'http://bottlepy.org/docs/dev/',
+         'views': 56},
+        {'title': 'Flask',
+         'url': 'http://flask.pocoo.org',
+         'views': 78}]
 
     cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
             'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
-            'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16} }
+            'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16}}
 
     for cat, cat_data in cats.items():
         c = add_cat(cat, views=cat_data['views'], likes=cat_data['likes'])
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['url'], p['views'])  # for chapter 6 we need the views
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print(f'- {c}: {p}')
 
+
 def add_page(cat, title, url, views=0):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
-    p.url=url
-    p.views=views
+    p.url = url
+    p.views = views
     p.save()
     return p
+
 
 def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
@@ -62,12 +75,8 @@ def add_cat(name, views=0, likes=0):
     c.save()
     return c
 
+
 # Start execution here!
 if __name__ == '__main__':
     print('Starting Rango population script...')
     populate()
-
-
-
-
-
